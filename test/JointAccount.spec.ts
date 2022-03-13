90740037fbfbb353b9c696e66d1b862e108791a5
Seth Charles <swcharl@protonmail.com>
2022-03-12 22:40:06 -0600

    Refactor previous factory design
    Refactor previous factory design. JointAccount contracts must
    register themselves with the JointAccountManager to be tracked.
    This refactor also cleans up the Proposal design and implementation.

--- test/JointAccount.spec.ts
+++ test/JointAccount.spec.ts
@@ -5,35 +5,56 @@ import config from "./vite.config.json";
 
 let provider: any;
 let deployer: any;
-let jointAccount: any;
+let manager: any;
+let compiledContracts: any;
 
 describe('test JointAccount', () => {
   before(async function() {
     provider = vite.localProvider();
-    deployer = vite.newAccount(config.networks.local.mnemonic, 0);
+    deployer = vite.newAccount(config.networks.local.mnemonic, 0, provider);
 
     // compile
-    const compiledContracts = await vite.compile('JointAccount.solpp');
+    compiledContracts = await vite.compile('JointAccount.solpp');
+    expect(compiledContracts).to.have.property('JointAccountManager');
     expect(compiledContracts).to.have.property('JointAccount');
 
-    // deploy
-    jointAccount = compiledContracts.JointAccount;
-    jointAccount.setDeployer(deployer).setProvider(provider);
-    await jointAccount.deploy({});
-    expect(jointAccount.address).to.be.a('string');
+    // deploy manager only
+    manager = compiledContracts.JointAccountManager;
+    manager.setDeployer(deployer).setProvider(provider);
+    await manager.deploy({});
+    expect(manager.address).to.be.a('string');
   });
 
   it('creates a joint account', async () => {
-    let alice = vite.newAccount(config.networks.local.mnemonic, 1);
-    let bob = vite.newAccount(config.networks.local.mnemonic, 2);
-    let charlie = vite.newAccount(config.networks.local.mnemonic, 3);
+    let alice = vite.newAccount(config.networks.local.mnemonic, 1, provider);
+    let bob = vite.newAccount(config.networks.local.mnemonic, 2, provider);
+    let charlie = vite.newAccount(config.networks.local.mnemonic, 3, provider);
 
     // create a joint account
     let members = [alice.address, bob.address, charlie.address];
-    await jointAccount.call('create', ['2', members], {caller: deployer});
+    let account = compiledContracts.JointAccount;
+    account.setDeployer(deployer).setProvider(provider);
+    await account.deploy({
+      tokenId: 'tti_5649544520544f4b454e6e40',
+      amount: '10000000000000000000',
+      params: [manager.address, members, '2']
+    });
+    expect(account.address).to.be.a('string');
+    expect(await manager.query('accounts', ['0'])).to.be.deep.equal([account.address]);
 
-    // verify the account
-    expect(await jointAccount.query('accounts', ['0'])).to.be.deep.equal(['2']);
-    expect(await jointAccount.query('getMembers', ['0'])).to.be.deep.equal([members]);
+    // user can submit proposal
+    // TODO: Why can't alice call this?
+    // await account.call('newProposal', [alice.address, 'tti_5649544520544f4b454e6e40', '10000000'], {caller: alice});
+    let destination = alice.address;
+    let VITE = 'tti_5649544520544f4b454e6e40';
+    await account.call('newProposal', [destination, VITE, '10000000'], {caller: deployer});
+    expect(await account.query('proposal', [])).to.be.deep.equal([
+      '1',
+      deployer.address, // FIXME: should be alice.address
+      destination,
+      VITE,
+      '10000000',
+      '0'
+    ]);
   });
 });
