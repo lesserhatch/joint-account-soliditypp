f99e9172e2afb5d47d56c9ee854d3b1676b91dc7
Seth Charles <swcharl@protonmail.com>
2022-03-14 11:25:16 -0500

    Add events and rename a few variables

--- test/JointAccount.spec.ts
+++ test/JointAccount.spec.ts
@@ -49,18 +49,40 @@ describe('test JointAccount', () => {
     account.setDeployer(deployer).setProvider(provider);
     await account.deploy({
       tokenId: VITE,
-      amount: '1234',
+      amount: '1000',
       params: [manager.address, members, '2']
     });
     expect(account.address).to.be.a('string');
     expect(await manager.query('accounts', ['0'])).to.be.deep.equal([account.address]);
 
+    let sentAmount = '234';
+    await deployer.sendToken(account.address, sentAmount);
+
+    let expectedBalance = '1234';
+    expect(await account.balance()).to.be.equal(expectedBalance);
+
     // member can submit proposal
     let destination = dave.address;
-    const proposalAmount = '1234';
+    let proposalAmount = '321';
+    let proposalId = '1';
     await account.call('newProposal', [destination, VITE, proposalAmount], {caller: alice});
     expect(await account.query('proposal', [])).to.be.deep.equal([
-      '1',
+      proposalId,
+      alice.address,
+      destination,
+      VITE,
+      proposalAmount,
+      '0'
+    ]);
+
+    // member can reject proposal
+    let rejectedProposalId = proposalId;
+    destination = dave.address;
+    proposalAmount = expectedBalance;
+    proposalId = '2'
+    await account.call('newProposal', [destination, VITE, proposalAmount], {caller: alice});
+    expect(await account.query('proposal', [])).to.be.deep.equal([
+      proposalId,
       alice.address,
       destination,
       VITE,
@@ -71,7 +93,7 @@ describe('test JointAccount', () => {
     // non-member cannot submit proposal
     await account.call('newProposal', [deployer.address, VITE, proposalAmount], {caller: deployer});
     expect(await account.query('proposal', [])).to.be.deep.equal([
-      '1',
+      proposalId,
       alice.address,
       destination,
       VITE,
@@ -80,9 +102,9 @@ describe('test JointAccount', () => {
     ]);
 
     // member can vote
-    await account.call('voteOnProposal', [], {caller: alice});
+    await account.call('approveProposal', [], {caller: alice});
     expect(await account.query('proposal', [])).to.be.deep.equal([
-      '1',
+      proposalId,
       alice.address,
       destination,
       VITE,
@@ -91,9 +113,9 @@ describe('test JointAccount', () => {
     ]);
 
     // ...but only once
-    await account.call('voteOnProposal', [], {caller: alice});
+    await account.call('approveProposal', [], {caller: alice});
     expect(await account.query('proposal', [])).to.be.deep.equal([
-      '1',
+      proposalId,
       alice.address,
       destination,
       VITE,
@@ -102,9 +124,9 @@ describe('test JointAccount', () => {
     ]);
 
     // non-member cannot vote
-    await account.call('voteOnProposal', [], {caller: deployer});
+    await account.call('approveProposal', [], {caller: deployer});
     expect(await account.query('proposal', [])).to.be.deep.equal([
-      '1',
+      proposalId,
       alice.address,
       destination,
       VITE,
@@ -113,9 +135,9 @@ describe('test JointAccount', () => {
     ]);
 
     // Final approval vote, and now proposal is reset
-    await account.call('voteOnProposal', [], {caller: bob});
+    await account.call('approveProposal', [], {caller: bob});
     expect(await account.query('proposal', [])).to.be.deep.equal([
-      '1',
+      proposalId,
       NULL_ADDRESS,
       NULL_ADDRESS,
       VITE,
@@ -131,12 +153,70 @@ describe('test JointAccount', () => {
     // Try a new proposal, but there are no funds, so it should revert
     await account.call('newProposal', [destination, VITE, "5678"], {caller: alice});
     expect(await account.query('proposal', [])).to.be.deep.equal([
-      '1',
+      proposalId,
       NULL_ADDRESS,
       NULL_ADDRESS,
       VITE,
       '0',
       '0',
     ]);
+
+    let events;
+
+    // check Received event
+    events = await account.getPastEvents('Received', {fromHeight: 0, toHeight: 0});
+    expect(events).to.be.an('array');
+    expect(events[0]).has.property('returnValues');
+    expect(events[0].returnValues).has.property('sender');
+    expect(events[0].returnValues).has.property('token');
+    expect(events[0].returnValues).has.property('amount');
+    expect(events[0].returnValues.sender).to.be.equals(deployer.address);
+    expect(events[0].returnValues.token).to.be.equals(VITE);
+    expect(events[0].returnValues.amount).to.be.equals(sentAmount);
+
+    // check ProposalCreated event
+    events = await account.getPastEvents('ProposalCreated', {fromHeight: 0, toHeight: 0});
+    expect(events).to.be.an('array');
+    expect(events[1]).has.property('returnValues');
+    expect(events[1].returnValues).has.property('id');
+    expect(events[1].returnValues).has.property('from');
+    expect(events[1].returnValues).has.property('to');
+    expect(events[1].returnValues).has.property('token');
+    expect(events[1].returnValues).has.property('amount');
+    expect(events[1].returnValues.id).to.be.equals(proposalId);
+    expect(events[1].returnValues.from).to.be.equals(alice.address);
+    expect(events[1].returnValues.to).to.be.equals(dave.address);
+    expect(events[1].returnValues.token).to.be.equals(VITE);
+    expect(events[1].returnValues.amount).to.be.equals(proposalAmount);
+
+    // check ProposalFunded event
+    events = await account.getPastEvents('ProposalRejected', {fromHeight: 0, toHeight: 0});
+    expect(events).to.be.an('array');
+    expect(events[0]).has.property('returnValues');
+    expect(events[0].returnValues).has.property('id');
+    expect(events[0].returnValues.id).to.be.equals(rejectedProposalId);
+
+    // check VoteReceived event
+    events = await account.getPastEvents('VoteReceived', {fromHeight: 0, toHeight: 0});
+    expect(events).to.be.an('array');
+    expect(events[0]).has.property('returnValues');
+    expect(events[0].returnValues).has.property('member');
+    expect(events[0].returnValues).has.property('proposalId');
+    expect(events[0].returnValues.member).to.be.equals(alice.address);
+    expect(events[0].returnValues.proposalId).to.be.equals(proposalId);
+
+    // check ProposalFunded event
+    events = await account.getPastEvents('ProposalFunded', {fromHeight: 0, toHeight: 0});
+    expect(events).to.be.an('array');
+    expect(events[0]).has.property('returnValues');
+    expect(events[0].returnValues).has.property('id');
+    expect(events[0].returnValues.id).to.be.equals(proposalId);
+
+    // check ProposalRemoved event
+    events = await account.getPastEvents('ProposalRemoved', {fromHeight: 0, toHeight: 0});
+    expect(events).to.be.an('array');
+    expect(events[0]).has.property('returnValues');
+    expect(events[0].returnValues).has.property('id');
+    expect(events[0].returnValues.id).to.be.equals(proposalId);
   });
 });
